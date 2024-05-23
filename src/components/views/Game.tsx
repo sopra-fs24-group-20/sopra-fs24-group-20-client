19cc5550038c6e2e79976b5a490a676aef8bcf07
Mirjam <mirjam.weibel@outlook.com>
2024-05-24 00:33:02 +0200

    char limit after

--- src/components/views/Game.tsx
+++ src/components/views/Game.tsx
@@ -16,7 +16,7 @@ const FormField = (props) => {
       <input
         className="game input"
         placeholder="enter here.."
-        value={props.value}
+        value={props.value.slice(0, 40)}
         onChange={(e) => props.onChange(e.target.value)}
         disabled={props.disabled}
       />
@@ -291,9 +291,10 @@ const Game = () => {
   }, [countdown]);
 
   const handleInputChange = (category: string, value: string) => {
+    const truncatedValue = value.slice(0, 40);
     setAnswers((prevAnswers) => ({
       ...prevAnswers,
-      [category]: value,
+      [category]: truncatedValue,
     }));
   };
 
